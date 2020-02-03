121c265d5214e39db8b212cc99a35a53e4951e77
vikramdevaraj <59722444+vikramdevaraj@users.noreply.github.com>
2020-02-04 01:34:24 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,14 @@
+package scratch;
+
+public class substring 
+{
+	public static void main(String[]args) {
+		String s1 ="core java";
+		int start=1;
+		char end=5;
+		System.out.println(start+end);
+		System.out.println(s1.substring(start,end));
+		
+	}
+
+}
